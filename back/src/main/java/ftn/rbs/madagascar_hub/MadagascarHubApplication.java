7a2f852139f1dbf74c75fa34d20a8ef3baec74ea
Nada Zaric <nada0zaric@gmail.com>
2024-06-23 16:15:07 +0200

    Add: Back project

---
+++ back/src/main/java/ftn/rbs/madagascar_hub/MadagascarHubApplication.java
@@ -0,0 +1,14 @@
+package ftn.rbs.madagascar_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MadagascarHubApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(MadagascarHubApplication.class, args);
+    }
+
+}
