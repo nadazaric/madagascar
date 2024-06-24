c62445ac4605022c027c3b8b3d833da86ee89349
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2024-06-24 20:01:33 +0200

    implemented method for finding cyclic roles

--- zanzibar/services/namespace.py
+++ zanzibar/services/namespace.py
@@ -17,6 +17,16 @@ def add(data):
     client.kv.put(consul_key, consul_value)
     get.cache_clear()
 
+def add(data):
+    namespace_dict = data
+
+    consul_key = namespace_dict['namespace']
+    relations_dict = namespace_dict.get('relations', {})
+    consul_value = json.dumps(relations_dict)
+
+    client.kv.put(consul_key, consul_value)
+    get.cache_clear()
+
 @lru_cache(maxsize=3)
 def get(namespace):
     try:
@@ -28,7 +38,22 @@ def get(namespace):
         return json.loads(data['Value'].decode('utf-8'))
     except Exception as e:
         raise Exception("Server error!")
+    
+# relations is value for key-value pair in consul db
+def get_roles_for_role(role, relations):
+    def resolve_role(current_role, resolved_roles):
+        if current_role in resolved_roles:
+            return
+        resolved_roles.add(current_role)
+        role_relations = relations.get(current_role, {})
+        if role_relations and 'union' in role_relations and role_relations['union']:
+            for sub_role in role_relations['union']:
+                if sub_role != "this":
+                    resolve_role(sub_role, resolved_roles)
 
+    resolved_roles = set()
+    resolve_role(role, resolved_roles)
+    return resolved_roles
 
     # relationship_key = f'relationships/{user_id}/{object_id}'
     # # version = get_latest_version(user_id, object_id) + 1
