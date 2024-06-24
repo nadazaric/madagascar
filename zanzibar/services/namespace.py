decf59dbece4be84c9c42b0f40b239ccc69c99fe
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2024-06-24 20:29:30 +0200

    removed duplicate and unnecessary code

--- zanzibar/services/namespace.py
+++ zanzibar/services/namespace.py
@@ -17,16 +17,6 @@ def add(data):
     client.kv.put(consul_key, consul_value)
     get.cache_clear()
 
-def add(data):
-    namespace_dict = data
-
-    consul_key = namespace_dict['namespace']
-    relations_dict = namespace_dict.get('relations', {})
-    consul_value = json.dumps(relations_dict)
-
-    client.kv.put(consul_key, consul_value)
-    get.cache_clear()
-
 @lru_cache(maxsize=3)
 def get(namespace):
     try:
@@ -54,28 +44,3 @@ def get_roles_for_role(role, relations):
     resolved_roles = set()
     resolve_role(role, resolved_roles)
     return resolved_roles
-
-    # relationship_key = f'relationships/{user_id}/{object_id}'
-    # # version = get_latest_version(user_id, object_id) + 1
-    
-    # relationship_data = {
-    #     "access_level": access_level,
-    #     "version": version,
-    #     "timestamp": datetime.utcnow().isoformat()
-    # }
-    
-    # # Store the relationship in ConsulDB
-    # client.kv.put(relationship_key, json.dumps(relationship_data))
-    
-    # # Store the versioned relationship
-    # versioned_key = f'versions/{user_id}_{object_id}/{version}'
-    # client.kv.put(versioned_key, json.dumps(relationship_data))
-
-
-
-# # Put a key-value pair in the Consul KV store
-# client.kv.put('foo', 'bar')
-
-# # Get a value from the Consul KV store
-# index, data = client.kv.get('foo')
-# print(data['Value'].decode())  # Output should be 'bar'
