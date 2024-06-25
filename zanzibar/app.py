e24128e45d036171db3be1295c589c6bf0b4baab
Nada Zaric <nada0zaric@gmail.com>
2024-06-25 11:22:40 +0200

    Remove: Dummy endpoint

--- zanzibar/app.py
+++ zanzibar/app.py
@@ -92,10 +92,6 @@ def get_app_key():
     app_key = generate_api_key()
     return jsonify({'message': 'Api-Key created.', 'api-key': app_key}), 201
 
-@app.route('/dummy', methods=['GET'])
-@require_api_key
-def dummy():
-    return jsonify({'message': 'Success.'}), 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=4000, debug=True)
