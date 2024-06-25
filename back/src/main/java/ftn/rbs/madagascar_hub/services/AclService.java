11522bc9738d2549e78c724897aa56028389344d
tince250 <t.s.mihajlovic25@gmail.com>
2024-06-25 12:19:24 +0200

    added validations for owner and file

--- back/src/main/java/ftn/rbs/madagascar_hub/services/AclService.java
+++ back/src/main/java/ftn/rbs/madagascar_hub/services/AclService.java
@@ -1,9 +1,13 @@
 package ftn.rbs.madagascar_hub.services;
 
 import ftn.rbs.madagascar_hub.dtos.AclDTO;
+import ftn.rbs.madagascar_hub.dtos.FrontAclDTO;
 import ftn.rbs.madagascar_hub.models.User;
+import ftn.rbs.madagascar_hub.models.File;
+import ftn.rbs.madagascar_hub.repositories.FileRepository;
 import ftn.rbs.madagascar_hub.repositories.UserRepository;
 import ftn.rbs.madagascar_hub.services.interfaces.IAclService;
+import ftn.rbs.madagascar_hub.services.interfaces.IUserService;
 import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,10 +32,12 @@ public class AclService implements IAclService {
     @Autowired
     private UserRepository allUsers;
 
-    private boolean doesFileExist(String filename) {
-        //TODO: implement
-        return true;
-    }
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private FileService fileService;
+
     private boolean doesUserExist(String username) {
         Optional<User> foundUser = allUsers.findByUsername(username);
         if (foundUser.isEmpty()) {
@@ -40,16 +46,18 @@ public class AclService implements IAclService {
 
         return true;
     }
-    private boolean isRequesterOwner(String filename) {
-        //TODO: implement
-        return true;
+    private boolean isRequesterOwner(File file) {
+        Long loggedUserId = userService.getCurrentUser().getId();
+        return file.getOwner().getId() == loggedUserId;
+    }
+
+    private void validateAclDto(FrontAclDTO dto, File file) {
+        if (!isRequesterOwner(file) || !doesUserExist(dto.getUser()))
+            throw new RuntimeException("ACL not valid.");
     }
 
     private ResponseEntity<?> sendRequestToZanzibar(AclDTO dto, String endpoint, HttpMethod httpMethod) {
         String url = String.format("%s/%s", zanzibarPath, endpoint);
-
-        dto.setUser("user:" + dto.getUser());
-        dto.setObject(zanzibarNamespace + ":" + dto.getObject());
 
         System.out.println(url);
 
@@ -60,34 +68,44 @@ public class AclService implements IAclService {
         return restTemplate.exchange(url, httpMethod, requestEntity, String.class);
     }
 
-    public ResponseEntity<?> add(AclDTO dto) {
-        if (!doesFileExist(dto.getObject()) || !isRequesterOwner(dto.getObject()) || !doesUserExist(dto.getUser()))
-            throw new RuntimeException("ACL not valid.");
+    private String formatObjectName(File file) {
+        return file.getName() + file.getId();
+    }
 
-        return sendRequestToZanzibar(dto, "acl", HttpMethod.POST);
+    public ResponseEntity<?> add(FrontAclDTO dto) {
+        File file = fileService.getFile(dto.getFileId());
+        AclDTO zanzibarDto = new AclDTO("user:" + dto.getUser(), dto.getRelation(), formatObjectName(file));
+        validateAclDto(dto, file);
+
+        return sendRequestToZanzibar(zanzibarDto, "acl", HttpMethod.POST);
     }
 
     @Override
-    public ResponseEntity<?> update(AclDTO dto) {
-        if (!doesFileExist(dto.getObject()) || !isRequesterOwner(dto.getObject()) || !doesUserExist(dto.getUser()))
-            throw new RuntimeException("ACL not valid.");
+    public ResponseEntity<?> update(FrontAclDTO dto) {
+        File file = fileService.getFile(dto.getFileId());
+        AclDTO zanzibarDto = new AclDTO("user:" + dto.getUser(), dto.getRelation(), formatObjectName(file));
+        validateAclDto(dto, file);
 
-        return sendRequestToZanzibar(dto, "acl", HttpMethod.PUT);
+        return sendRequestToZanzibar(zanzibarDto, "acl", HttpMethod.PUT);
     }
 
     @Override
-    public ResponseEntity<?> delete(AclDTO dto) {
-        if (!doesFileExist(dto.getObject()) || !isRequesterOwner(dto.getObject()) || !doesUserExist(dto.getUser()))
-            throw new RuntimeException("ACL not valid.");
+    public ResponseEntity<?> delete(FrontAclDTO dto) {
+        File file = fileService.getFile(dto.getFileId());
+        AclDTO zanzibarDto = new AclDTO("user:" + dto.getUser(), dto.getRelation(), formatObjectName(file));
+        validateAclDto(dto, file);
 
-        return sendRequestToZanzibar(dto, "acl", HttpMethod.DELETE);
+        return sendRequestToZanzibar(zanzibarDto, "acl", HttpMethod.DELETE);
     }
 
     @Override
-    public ResponseEntity<?> check(AclDTO dto) {
-        if (!doesFileExist(dto.getObject()) || !isRequesterOwner(dto.getObject()) || !doesUserExist(dto.getUser()))
+    public ResponseEntity<?> check(FrontAclDTO dto) {
+        File file = fileService.getFile(dto.getFileId());
+        AclDTO zanzibarDto = new AclDTO("user:" + dto.getUser(), dto.getRelation(), formatObjectName(file));
+
+        if (!doesUserExist(dto.getUser()))
             throw new RuntimeException("ACL not valid.");
 
-        return sendRequestToZanzibar(dto, "acl/check", HttpMethod.POST);
+        return sendRequestToZanzibar(zanzibarDto, "acl/check", HttpMethod.POST);
     }
 }
